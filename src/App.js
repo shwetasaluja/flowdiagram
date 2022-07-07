3c56120f44425cd439ab398fe0cf0eb6748315b4
shwetasaluja <testmss42@gmail.com>
2022-07-07 12:29:52 +0530

    dynamic flow chart with list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import Title from './components/title';
 import Node from './components/node';
 
+const canvasStyle = {
+  position: 'relative',
+  height: '60vh',
+  background: 'white',
+  display: 'flex',
+  justifyContent: 'space-evenly',
+  alignItems: 'center',
+};
+
 function App() {
   // const stepsArray = [0, 1, 2];
   
@@ -10,7 +19,9 @@ function App() {
     <div className="App">
       <header className="My-header">
         <Title text="React Flow Diagram"/>
-        <Node color="pink"/>
+        <div style={canvasStyle} id="canvas">
+          <Node color="pink"/>
+        </div>
         {/* {stepsArray.map((item, index) => <Node key={item} color="pink"/> )} */}
       </header>
     </div>
--- src/components/node.js
+++ src/components/node.js
@@ -3,86 +3,52 @@ import { useState } from "react";
 import { Fab } from '@mui/material';
 import Xarrow from 'react-xarrows';
 
-const canvasStyle = {
-  position: 'relative',
-  height: '60vh',
-  background: 'white',
-  display: 'flex',
-  justifyContent: 'space-evenly',
-  alignItems: 'center',
-};
-
 export default function Node(props) {
-  const [step1, setStep1] = useState("");
-  const [step2, setStep2] = useState("");
-  const [step3, setStep3] = useState("");
-  const [step4, setStep4] = useState("");
+  const itemsArray = [
+    { id: '1', name: 'Step 1', duration: 'Time: 2.10 hrs' },
+    { id: '2', name: 'Step 2', duration: 'Time: 1.50 hrs' },
+    { id: '3', name: 'Step 3', duration: 'Time: 1.10 hrs' },
+    { id: '4', name: 'Step 4', duration: 'Waiting' },
+    { id: '5', name: 'Step 5', duration: 'Waiting' }
+  ]
+
+  const [stepInfo, setStepInfo] = useState("");
+
+  const mouseEnter = (data) => {
+    setStepInfo(data.id);
+    console.log(stepInfo, data, typeof stepInfo);
+  }
+  const mouseLeave = (data) => {
+    setStepInfo(""); 
+  }
 
   return (
-    <div style={canvasStyle} id="canvas">
-      <Fab style={{ height: "22%", width: "11%" }} id={'comp1'} color="success" 
-        onMouseEnter={() => {
-          setStep1("Time: 2.10 hrs");
-        }}
-        onMouseLeave={() => {
-          setStep1("");
-        }}
-      > Step 1 <br />
-      {step1.length > 0 &&
-        <div style={{ fontSize: "10px" }}>
-          ({step1})
-        </div>
-      }
-      </Fab>
+    <>
+      {itemsArray.map(item => (
+        // <li key={item.id}> {item.name}</li>
+        <>
+          <Fab id={item.id} style={{ height: "22%", width: "11%" }} 
+          color={item.duration === 'Waiting' ? "info" : "success" }
+            onMouseEnter={
+              () => {
+                mouseEnter(item);
+              }}
+            onMouseLeave={
+              () => {
+                mouseLeave(item);
+              }} > 
+            Step {item.id} <br />
+            
+            { item.id === stepInfo && 
+              (item.duration)
+            }
+          </Fab>
 
-      <Fab style={{ height: "22%", width: "11%" }} id={'comp2'} color="success" variant="circular" 
-        onMouseEnter={() => {
-          setStep2("Time: 4.05 hrs");
-        }}
-        onMouseLeave={() => {
-          setStep2("");
-        }}
-      > Step 2
-      {step2.length > 0 &&
-        <div style={{ fontSize: "10px" }}>
-          ({step2})
-        </div>
-      }
-      </Fab>
-
-      <Fab style={{ height: "22%", width: "11%" }} id={'comp3'} color="info" variant="circular"
-        onMouseEnter={() => {
-          setStep3("Waiting");
-        }}
-        onMouseLeave={() => {
-          setStep3("");
-        }}
-      > Step 3
-      {step3.length > 0 &&
-        <div style={{ fontSize: "10px" }}>
-          ({step3})
-        </div>
-      }
-      </Fab>
-
-      <Fab style={{ height: "22%", width: "11%" }} id={'comp4'} color="info" variant="circular" 
-        onMouseEnter={() => {
-          setStep4("Waiting");
-        }}
-        onMouseLeave={() => {
-          setStep4("");
-        }}
-      > Step 4
-      {step4.length > 0 &&
-        <div style={{ fontSize: "10px" }}>
-          ({step4})
-        </div>
-      }
-      </Fab>
-
-      <Xarrow start={'comp1'} end={'comp2'} />
-      <Xarrow color="grey" start={'comp2'} end={'comp3'} />
-      <Xarrow color="grey" start={'comp3'} end={'comp4'} />
-    </div>
+          { item.id !== '1' && 
+            <Xarrow start={String(parseInt(item.id)-1)} end={item.id} />
+          }
+        </>
+        ))}
+    </>
   )
 }
